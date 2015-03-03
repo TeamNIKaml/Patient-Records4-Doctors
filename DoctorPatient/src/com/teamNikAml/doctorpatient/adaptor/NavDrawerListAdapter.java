bfaa543e9ff810b51e7acaefe5965b940e6218b0
Nikhil <nikhil.vijaykumar@gmail.com>
2015-03-03 18:19:24 +0530

    Adding icon and changing menu name 6:19pm 3-3-2015

--- DoctorPatient/src/com/teamNikAml/doctorpatient/adaptor/NavDrawerListAdapter.java
+++ DoctorPatient/src/com/teamNikAml/doctorpatient/adaptor/NavDrawerListAdapter.java
@@ -2,8 +2,6 @@ package com.teamNikAml.doctorpatient.adaptor;
 
 import java.util.ArrayList;
 
-import com.teamNikAml.doctorpatient.model.NavDrawerItem;
-
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -13,59 +11,61 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
+import com.teamNikAml.doctorpatient.activity.R;
+import com.teamNikAml.doctorpatient.model.NavDrawerItem;
 
 public class NavDrawerListAdapter extends BaseAdapter {
 
-    private Context context;
-    private ArrayList<NavDrawerItem> navDrawerItems;
+	private Context context;
+	private ArrayList<NavDrawerItem> navDrawerItems;
 
-    public NavDrawerListAdapter(Context context,
-	    ArrayList<NavDrawerItem> navDrawerItems) {
-	this.context = context;
-	this.navDrawerItems = navDrawerItems;
-    }
-
-    @Override
-    public int getCount() {
-	return navDrawerItems.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-	return navDrawerItems.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-	return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-	if (convertView == null) {
-	    LayoutInflater mInflater = (LayoutInflater) context
-		    .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-	    convertView = mInflater.inflate(R.layout.drawer_list_item, parent,false);
+	public NavDrawerListAdapter(Context context,
+			ArrayList<NavDrawerItem> navDrawerItems) {
+		this.context = context;
+		this.navDrawerItems = navDrawerItems;
 	}
 
-	ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
-	TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
-	TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
-
-	imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
-	txtTitle.setText(navDrawerItems.get(position).getTitle());
-
-	// displaying count
-	// check whether it set visible or not
-	if (navDrawerItems.get(position).getCounterVisibility()) {
-	    txtCount.setText(navDrawerItems.get(position).getCount());
-	} else {
-	    // hide the counter view
-	    txtCount.setVisibility(View.GONE);
+	@Override
+	public int getCount() {
+		return navDrawerItems.size();
 	}
 
-	return convertView;
-    }
+	@Override
+	public Object getItem(int position) {
+		return navDrawerItems.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			LayoutInflater mInflater = (LayoutInflater) context
+					.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+			convertView = mInflater.inflate(R.layout.drawer_list_item, parent,
+					false);
+		}
+
+		ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+		TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
+		TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
+
+		imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
+		txtTitle.setText(navDrawerItems.get(position).getTitle());
+
+		// displaying count
+		// check whether it set visible or not
+		if (navDrawerItems.get(position).getCounterVisibility()) {
+			txtCount.setText(navDrawerItems.get(position).getCount());
+		} else {
+			// hide the counter view
+			txtCount.setVisibility(View.GONE);
+		}
+
+		return convertView;
+	}
 
 }
