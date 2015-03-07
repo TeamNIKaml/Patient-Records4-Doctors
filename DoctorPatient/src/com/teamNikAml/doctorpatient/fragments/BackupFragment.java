18e4beb918df91e58ff779616d95a5c9fb7c873f
CodingManes <ta.amaldev@gmail.com>
2015-03-07 18:46:58 +0530

    google service lib added..

--- DoctorPatient/src/com/teamNikAml/doctorpatient/fragments/BackupFragment.java
+++ DoctorPatient/src/com/teamNikAml/doctorpatient/fragments/BackupFragment.java
@@ -1,11 +1,15 @@
 package com.teamNikAml.doctorpatient.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.teamNikAml.doctorpatient.activity.R;
 
 public class BackupFragment extends Fragment {
@@ -21,4 +25,42 @@ public class BackupFragment extends Fragment {
 
 		return rootView;
 	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+/*
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}*/
+	
+	
 }
