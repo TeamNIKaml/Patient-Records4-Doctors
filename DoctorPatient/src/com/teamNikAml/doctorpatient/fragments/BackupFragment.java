71d930210ab73fd184917504ac80b9a1ef27f186
CodingManes <ta.amaldev@gmail.com>
2015-03-23 22:37:29 +0530

    backup phone internal mem

--- DoctorPatient/src/com/teamNikAml/doctorpatient/fragments/BackupFragment.java
+++ DoctorPatient/src/com/teamNikAml/doctorpatient/fragments/BackupFragment.java
@@ -1,8 +1,14 @@
 package com.teamNikAml.doctorpatient.fragments;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +31,30 @@ public class BackupFragment extends Fragment {
 		View rootView = inflater.inflate(R.layout.fragment_backup, container,
 				false);
 
+		try {
+	        File sd = Environment.getExternalStorageDirectory();
+	        File data = Environment.getDataDirectory();
+
+	        if (data.canWrite()) {
+	            String currentDBPath = "//data//"+ "com.teamNikAml.doctorpatient.activity" +"//databases//"+"PatientRecordsDatabase";
+	            String backupDBPath = "DatabaseBackup";
+	            File currentDB = new File(data, currentDBPath);
+	            File backupDB = new File(data, backupDBPath);
+
+	            if (currentDB.exists()) {
+	                FileInputStream fileInputStream = new FileInputStream(currentDB);
+					FileChannel src = fileInputStream.getChannel();
+	                FileOutputStream fileOutputStream = new FileOutputStream(backupDB);
+					FileChannel dst = fileOutputStream.getChannel();
+	                dst.transferFrom(src, 0, src.size());
+	                src.close();
+	                dst.close();
+	                fileInputStream.close();
+	                fileOutputStream.close();
+	            }
+	        }
+	    } catch (Exception e) {
+	    }
 		return rootView;
 	}
 
