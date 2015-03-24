4c91943e4b05d00744a9e779f4126f039e5a3c01
CodingManes <ta.amaldev@gmail.com>
2015-03-24 12:32:10 +0530

    permission added and write external sd card

--- DoctorPatient/src/com/teamNikAml/doctorpatient/fragments/BackupFragment.java
+++ DoctorPatient/src/com/teamNikAml/doctorpatient/fragments/BackupFragment.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.channels.FileChannel;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,12 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.teamNikAml.doctorpatient.activity.R;
-import com.teamNikAml.doctorpatient.database.DatabaseConfig;
-
-
-/*import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;*/
 
 public class BackupFragment extends Fragment {
 
@@ -33,26 +29,98 @@ public class BackupFragment extends Fragment {
 				false);
 
 		try {
-			
-			System.out.println("dfsdfsddsds");
-			
 	        File sd = Environment.getExternalStorageDirectory();
 	        File data = Environment.getDataDirectory();
 
-	     
+	        if (sd.canWrite()) {
+	            String currentDBPath = "//data//"+ "com.teamNikAml.doctorpatient.activity" +"//databases//"+"PatientRecordsDatabase";
+	            String backupDBPath = "PatientRecordBackup";
+	            File currentDB = new File(data, currentDBPath);
+	            File backupDB = new File(sd, backupDBPath);
+
+	            if (currentDB.exists()) {
+	                FileInputStream fileInputStream = new FileInputStream(currentDB);
+					FileChannel src = fileInputStream.getChannel();
+	                FileOutputStream fileOutputStream = new FileOutputStream(backupDB);
+					FileChannel dst = fileOutputStream.getChannel();
+	                dst.transferFrom(src, 0, src.size());
+	                src.close();
+	                dst.close();
+	                fileInputStream.close();
+	                fileOutputStream.close();
+	            }
+	        }
+	    } catch (Exception e) {
+	    }
+		
+		
+		Calendar cal = Calendar.getInstance();
+        Date currentLocalTime = cal.getTime();
+        SimpleDateFormat date = new SimpleDateFormat("HH:mm"); 
+        String localTime = date.format(currentLocalTime); 
+        System.out.println(localTime);
+		
+		return rootView;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	
+}
+
+
+
+/*package com.teamNikAml.doctorpatient.fragments;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamNikAml.doctorpatient.activity.R;
+
+
+
+public class BackupFragment extends Fragment {
+
+	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_backup, container,
+				false);
+
+		try {
+     
 	            File currentDBPath = getActivity().getApplicationContext().getDatabasePath("PatientRecordsDatabase");
 	            
 	            File backupDBPath ;
 	            backupDBPath = new File(getActivity().getApplicationContext().getFilesDir() + "/" + "DatabaseBackup" );
 	    		
-	            
-	            
-	            
-	            System.out.println(currentDBPath.getPath());
-	          
-	            //File currentDB = new File(data, currentDBPath);
-	           // File backupDB = new File(data, backupDBPath);
-
 	            if (currentDBPath.exists()) {
 	                FileInputStream fileInputStream = new FileInputStream(currentDBPath);
 					FileChannel src = fileInputStream.getChannel();
@@ -91,24 +159,7 @@ public class BackupFragment extends Fragment {
 		// TODO Auto-generated method stub
 		super.onResume();
 	}
-/*
-	@Override
-	public void onConnectionFailed(ConnectionResult arg0) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void onConnected(Bundle arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onConnectionSuspended(int arg0) {
-		// TODO Auto-generated method stub
-		
-	}*/
-	
 	
 }
+*/
